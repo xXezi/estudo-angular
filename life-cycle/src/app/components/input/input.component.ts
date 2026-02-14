4d428e031ec7024c32d30a926807d8d4ec15e38e
xXezi <willroberti04@hotmail.com>
2026-02-14 18:53:53 -0300

    implementado gancho onChanges, diferenciado o OnInit e OnChanges e criado e adicionado novos itens na lista

--- life-cycle/src/app/components/input/input.component.ts
+++ life-cycle/src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
-  constructor() { }
+  valorItem!: string;  
+
+  constructor(private listaService: ListaDeCompraService) { }
 
   ngOnInit(): void { }
+
+  adicionarItem(){
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo(){
+    this.valorItem = "";
+  }
 }
