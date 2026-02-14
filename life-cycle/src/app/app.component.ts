e424650d761e0b1012d99fffe1a6b450aeab39a1
xXezi <willroberti04@hotmail.com>
2026-02-14 17:44:16 -0300

    ciclo de vida angular, hooks em angular e usado o OnInit

--- life-cycle/src/app/app.component.ts
+++ life-cycle/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
+  listaDeCompras!: Array<Item>
 
-  constructor() { }
+  constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompras = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompras);
+  }
 }
