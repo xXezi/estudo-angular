d35b83e4431efe9d4e64e6b14e09ac550e43ff12
xXezi <willroberti04@hotmail.com>
2026-02-15 11:08:24 -0300

    implementado o localStorage, utilizado o doCheck e utilizado o style-binding

--- life-cycle/src/app/components/item/item.component.ts
+++ life-cycle/src/app/components/item/item.component.ts
@@ -25,4 +25,13 @@ export class ItemComponent implements OnInit, OnChanges {
     this.emitindoItemParaEditar.emit(this.item);
   }
 
+  checarItem(){
+    if(this.item.comprado == true){
+      this.item.comprado = false;
+    }
+    else {
+      this.item.comprado = true;
+    }
+  }
+
 }
