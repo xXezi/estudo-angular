2e730373f070f0e396a064367857443f846251b4
xXezi <willroberti04@hotmail.com>
2026-04-04 19:26:46 -0300

    criado o componente contato

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     RouterOutlet, 
     ContainerComponent, 
     CabecalhoComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ContatoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
