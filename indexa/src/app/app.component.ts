ee5bae8ea623d832965fdddd885ce4d010b77c07
xXezi <willroberti04@hotmail.com>
2026-04-05 22:01:15 -0300

    exibindo contatos pela letra inicial

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -6,6 +6,14 @@ import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component'
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
 
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -22,4 +30,11 @@ import { ContatoComponent } from './componentes/contato/contato.component';
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra: string) : Contato[]{
+    return this.contatos.filter(contato => {
+      return contato.nome.toLocaleLowerCase().startsWith(letra)
+    })
+  }
 }
